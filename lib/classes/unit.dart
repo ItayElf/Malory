93154b43aa79e6ddf2872d57fe7a3faa23540538
ItayElf <thh123321@gmail.com>
2021-12-14 14:15:43 +0200

    added unit class and endpoints

---
+++ lib/classes/unit.dart
@@ -0,0 +1,58 @@
+import 'package:malory/classes/attribute.dart';
+
+class Unit {
+  Unit.fromJson(Map<String, dynamic> json)
+      : category = json["category"],
+        name = json["name"],
+        description = json["description"],
+        clas = json["clas"],
+        subclass = json["subclass"],
+        cost = json["cost"],
+        men = json["men"],
+        weight = json["weight"],
+        hitpoints = json["hitpoints"],
+        armor = json["armor"],
+        shield = json["shield"],
+        morale = json["morale"],
+        speed = json["speed"],
+        meleeAttack = json["melee_attack"],
+        defense = json["defense"],
+        damage = json["damage"],
+        ap = json["ap"],
+        charge = json["charge"],
+        ammunition = json["ammunition"],
+        range = json["range"],
+        rangedAttack = json["ranged_attack"],
+        rangedDamage = json["ranged_damage"],
+        rangedAp = json["ranged_ap"],
+        attributes = json["attributes"]
+            .map<Attribute>((e) => Attribute.fromJson(e))
+            .toList(),
+        idx = json["idx"];
+
+  String category;
+  String name;
+  String description;
+  String clas;
+  String subclass;
+  double cost;
+  int men;
+  double weight;
+  int hitpoints;
+  int armor;
+  double shield;
+  int morale;
+  int speed;
+  int meleeAttack;
+  int defense;
+  int damage;
+  int ap;
+  int charge;
+  int ammunition;
+  int range;
+  int rangedAttack;
+  int rangedDamage;
+  int rangedAp;
+  List<Attribute> attributes;
+  int idx;
+}
--- lib/services/client.dart
+++ lib/services/client.dart
@@ -6,6 +6,7 @@ import 'package:http/http.dart';
 
 import 'package:http/http.dart' as http;
 import 'package:malory/classes/attribute.dart';
+import 'package:malory/classes/unit.dart';
 import 'package:malory/utils.dart';
 
 class Client {
@@ -28,12 +29,123 @@ class Client {
     }
   }
 
-  static Future<String> apiVersion() async {
-    Response response = await http.get(Uri.http(_baseUrl, "/api/version"));
+  static Future<List<Attribute>> getAllAttributes() async {
+    Response response = await http.get(
+      Uri.http(
+        _baseUrl,
+        "/api/attributes",
+      ),
+    );
     if (response.statusCode != 200) {
       throw HttpException(response.body);
     }
-    return response.body;
+    List<dynamic> jsonList = jsonDecode(response.body);
+    return jsonList.map((e) => Attribute.fromJson(e)).toList();
+  }
+
+  static Future<Attribute> getAttribute(String name) async {
+    Response response = await http.get(
+      Uri.http(
+        _baseUrl,
+        "/api/attribute/$name",
+      ),
+    );
+    if (response.statusCode != 200) {
+      throw HttpException(response.body);
+    }
+    return Attribute.fromJson(jsonDecode(response.body));
+  }
+
+  static Future<List<Unit>> hasAttributeFull(String name) async {
+    Response response = await http.get(
+      Uri.http(
+        _baseUrl,
+        "/api/has_attribute/$name",
+        {
+          "full": "true",
+        },
+      ),
+    );
+    if (response.statusCode != 200) {
+      throw HttpException(response.body);
+    }
+    List<dynamic> jsonList = jsonDecode(response.body);
+    return jsonList.map((e) => Unit.fromJson(e)).toList();
+  }
+
+  static Future<List<String>> hasAttribute(String name) async {
+    Response response = await http.get(
+      Uri.http(
+        _baseUrl,
+        "/api/has_attribute/$name",
+        {
+          "full": "false",
+        },
+      ),
+    );
+    if (response.statusCode != 200) {
+      throw HttpException(response.body);
+    }
+    List<dynamic> jsonList = jsonDecode(response.body);
+    return jsonList.map((e) => e.toString()).toList();
+  }
+
+  static Future<List<Unit>> getAllUnits() async {
+    Response response = await http.get(
+      Uri.http(
+        _baseUrl,
+        "/api/units",
+      ),
+    );
+    if (response.statusCode != 200) {
+      throw HttpException(response.body);
+    }
+    List<dynamic> jsonList = jsonDecode(response.body);
+    return jsonList.map((e) => Unit.fromJson(e)).toList();
+  }
+
+  static Future<Unit> getUnit(String name) async {
+    Response response = await http.get(
+      Uri.http(
+        _baseUrl,
+        "/api/unit/$name",
+      ),
+    );
+    if (response.statusCode != 200) {
+      throw HttpException(response.body);
+    }
+    return Unit.fromJson(jsonDecode(response.body));
+  }
+
+  static Future<List<String>> getCategories([bool playable = true]) async {
+    Response response = await http.get(
+      Uri.http(
+        _baseUrl,
+        "/api/categories",
+        {
+          "playable": playable.toString(),
+        },
+      ),
+    );
+    if (response.statusCode != 200) {
+      throw HttpException(response.body);
+    }
+    List<dynamic> jsonList = jsonDecode(response.body);
+    return jsonList.map((e) => e.toString()).toList();
+  }
+
+  static Future<List<String>> getNations() async {
+    Response response = await http.get(
+      Uri.http(
+        _baseUrl,
+        "/api/nations",
+      ),
+    );
+    if (response.statusCode != 200) {
+      throw HttpException(response.body);
+    }
+    List<dynamic> jsonList = jsonDecode(response.body);
+    return jsonList.map((e) => e.toString()).toList();
   }
 
   static Future<bool> registerUser(String username, String password) async {
@@ -77,30 +189,11 @@ class Client {
     return isOk;
   }
 
-  static Future<List<Attribute>> getAllAttributes() async {
-    Response response = await http.get(
-      Uri.http(
-        _baseUrl,
-        "/api/attributes",
-      ),
-    );
+  static Future<String> apiVersion() async {
+    Response response = await http.get(Uri.http(_baseUrl, "/api/version"));
     if (response.statusCode != 200) {
       throw HttpException(response.body);
     }
-    List<dynamic> jsonList = jsonDecode(response.body);
-    return jsonList.map((e) => Attribute.fromJson(e)).toList();
-  }
-
-  static Future<Attribute> getAttribute(String name) async {
-    Response response = await http.get(
-      Uri.http(
-        _baseUrl,
-        "/api/attribute/$name",
-      ),
-    );
-    if (response.statusCode != 200) {
-      throw HttpException(response.body);
-    }
-    return Attribute.fromJson(jsonDecode(response.body));
+    return response.body;
   }
 }
