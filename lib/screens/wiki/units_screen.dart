cb57d7e7950ca26caf5f9e24a5c92844a6fcddf5
ItayElf <thh123321@gmail.com>
2021-12-16 15:02:34 +0200

    fixed back button

--- lib/screens/wiki/units_screen.dart
+++ lib/screens/wiki/units_screen.dart
@@ -39,57 +39,53 @@ class _UnitsScreenState extends State<UnitsScreen> {
               colors: [Color(0xff0d0d0d), Color(0xff013a40), Color(0xff0d0d0d)],
             ),
           ),
-          child: Stack(
-            children: [
-              Positioned(
-                top: convert(40),
-                left: convert(40),
-                child: InkWell(
-                  child: Icon(
-                    Icons.arrow_back,
-                    color: Colors.white,
-                    size: convert(64),
-                  ),
-                  onTap: Navigator.of(context).pop,
-                ),
-              ),
-              Align(
-                alignment: Alignment.center,
-                child: SingleChildScrollView(
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: convert(15)),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Text(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: convert(15)),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      InkWell(
+                        child: Icon(
+                          Icons.arrow_back,
+                          color: Colors.white,
+                          size: convert(64),
+                        ),
+                        onTap: Navigator.of(context).pop,
+                      ),
+                      Expanded(
+                        child: Text(
                           "Units",
                           style: Theme.of(context).textTheme.headline1,
                           textAlign: TextAlign.center,
                         ),
-                        Row(
-                          children: [
-                            Flexible(
-                              child: GridView.count(
-                                shrinkWrap: true,
-                                crossAxisCount: 4,
-                                crossAxisSpacing: convert(25),
-                                mainAxisSpacing: convert(25),
-                                children: filteredUnits
-                                    .map((e) => UnitCard(unit: e))
-                                    .toList(),
-                              ),
-                            ),
-                            SizedBox(
-                              width: convert(300),
-                            )
-                          ],
-                        )
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                ),
-              )
-            ],
+                  Row(
+                    children: [
+                      Flexible(
+                        child: GridView.count(
+                          shrinkWrap: true,
+                          crossAxisCount: 4,
+                          crossAxisSpacing: convert(25),
+                          mainAxisSpacing: convert(25),
+                          children: filteredUnits
+                              .map((e) => UnitCard(unit: e))
+                              .toList(),
+                        ),
+                      ),
+                      SizedBox(
+                        width: convert(300),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
           ),
         ),
       ),
